285432e43d5ddb709f05d34c5a801803b0825330
Marica Pasquali <marica.pasquali@studio.unibo.it>
2021-03-06 17:52:04 +0100

    implement CircularList and create his tests (step 1)

---
+++ pps-lab01-tdd/src/lab01/tdd/CircularListImpl.java
@@ -0,0 +1,68 @@
+package lab01.tdd;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Optional;
+
+public class CircularListImpl implements CircularList {
+
+    private ListIterator<Integer> iterator;
+    private final List<Integer> circularList;
+
+    public CircularListImpl() {
+        this.circularList = new LinkedList<>();
+        this.iterator = circularList.listIterator();
+    }
+
+    @Override
+    public void add(int element) {
+        this.circularList.add(element);
+        this.iterator = circularList.listIterator();
+    }
+
+    @Override
+    public int size() {
+        return this.circularList.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.circularList.isEmpty();
+    }
+
+    @Override
+    public Optional<Integer> next() {
+        if (this.iterator.hasNext()) {
+            return Optional.of(this.iterator.next());
+        } else if (this.size() > 0) {
+            this.iterator = circularList.listIterator();
+            return Optional.of(this.iterator.next());
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public Optional<Integer> previous() {
+        if (this.iterator.hasPrevious()) {
+            return Optional.of(this.iterator.previous());
+        } else if (this.size() > 0) {
+            var lastIndex = this.size() - 1;
+            this.iterator = circularList.listIterator(lastIndex);
+            return Optional.of(this.circularList.get(lastIndex));
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public void reset() {
+        this.iterator = circularList.listIterator();
+    }
+
+    @Override
+    public Optional<Integer> next(SelectStrategy strategy) {
+        return Optional.empty();
+    }
+}
--- pps-lab01-tdd/test/CircularListTest.java
+++ pps-lab01-tdd/test/CircularListTest.java
@@ -1,17 +1,106 @@
+import lab01.tdd.CircularList;
+import lab01.tdd.CircularListImpl;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
 
 /**
  * The test suite for testing the CircularList implementation
  */
 public class CircularListTest {
 
-    //TODO: test implementation
+    private CircularList circularList;
 
-    @Disabled
-    @Test public void testTodo(){
-        Assertions.fail();
+    @BeforeEach
+    void setUp() {
+        circularList = new CircularListImpl();
     }
 
+    @Test
+    void testAdd() {
+        circularList.add(20);
+        Assertions.assertEquals(Optional.of(20), circularList.next());
+    }
+
+    @Test
+    void testSize() {
+        Assertions.assertEquals(0, circularList.size());
+    }
+
+    @Test
+    void testIsEmpty() {
+        Assertions.assertTrue(circularList.isEmpty());
+    }
+
+    @Test
+    void testNextIfEmpty() {
+        Assertions.assertEquals(Optional.empty(), circularList.next());
+    }
+
+    @Test
+    void testNext() {
+        circularList.add(20);
+        circularList.add(40);
+        circularList.add(2);
+        Assertions.assertEquals(Optional.of(20), circularList.next());
+        Assertions.assertEquals(Optional.of(40), circularList.next());
+        Assertions.assertEquals(Optional.of(2), circularList.next());
+    }
+
+    @Test
+    void testNextOnLast() {
+        circularList.add(20);
+        circularList.add(40);
+        circularList.add(2);
+
+        Assertions.assertEquals(Optional.of(20), circularList.next());
+        Assertions.assertEquals(Optional.of(40), circularList.next());
+        Assertions.assertEquals(Optional.of(2), circularList.next());
+
+        Assertions.assertEquals(Optional.of(20), circularList.next());
+        Assertions.assertEquals(Optional.of(40), circularList.next());
+    }
+
+    @Test
+    void testPreviousIfEmpty() {
+        Assertions.assertEquals(Optional.empty(), circularList.previous());
+    }
+
+    @Test
+    void testPrevious() {
+        circularList.add(20);
+        circularList.add(40);
+        circularList.add(2);
+        Assertions.assertEquals(Optional.of(20), circularList.next());
+        Assertions.assertEquals(Optional.of(40), circularList.next());
+
+        Assertions.assertEquals(Optional.of(40), circularList.previous());
+        Assertions.assertEquals(Optional.of(20), circularList.previous());
+    }
+
+    @Test
+    void testPreviousOnFirst() {
+        circularList.add(20);
+        circularList.add(40);
+        circularList.add(2);
+        Assertions.assertEquals(Optional.of(2), circularList.previous());
+        Assertions.assertEquals(Optional.of(40), circularList.previous());
+        Assertions.assertEquals(Optional.of(20), circularList.previous());
+
+        Assertions.assertEquals(Optional.of(2), circularList.previous());
+    }
+
+    @Test
+    void testReset() {
+        circularList.add(20);
+        circularList.add(40);
+        circularList.add(2);
+
+        Assertions.assertEquals(Optional.of(20), circularList.next());
+        Assertions.assertEquals(Optional.of(40), circularList.next());
+        circularList.reset();
+        Assertions.assertEquals(Optional.of(20), circularList.next());
+    }
 }
